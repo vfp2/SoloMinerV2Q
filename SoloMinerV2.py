af2f25a0f941c9a6d1928e39ff367ff9497b2677
Wandering Consciousness <10329002+Wandering-Consciousness@users.noreply.github.com>
2024-09-20 08:42:55 +1200

    chore: refactor Context class into main file

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -13,8 +13,27 @@ from signal import SIGINT , signal
 import requests
 from colorama import Back , Fore , Style
 
-import context as ctx
+class Context:
+    def __init__(self):
+        fShutdown = False
+        listfThreadRunning = [False] * 2
+        local_height = 0
+        nHeightDiff = {}
+        updatedPrevHash = None
+        job_id = None
+        prevhash = None
+        coinb1 = None
+        coinb2 = None
+        merkle_branch = None
+        version = None
+        nbits = None
+        ntime = None
+        clean_jobs = None
+        sub_details = None
+        extranonce1 = None
+        extranonce2_size = None
 
+ctx = Context()
 
 sock = None
 
